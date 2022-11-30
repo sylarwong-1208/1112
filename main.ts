69e6d1b0ecc347d46f58eb858fcc4c2a23bbfccd
sylarwong-1208 <119169132+sylarwong-1208@users.noreply.github.com>
2022-12-01 00:25:15 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    led.stopAnimation()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Hello!")
+})
+basic.forever(function () {
+	
+})
